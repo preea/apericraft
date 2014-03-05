1a304e2fde4175587969ad2f1f7d17353486b125
preea <preea03@gmail.com>
2014-03-05 18:59:40 -0500

    Initialization for forge and banner update.

---
+++ Common/com/apericraft/apericraft/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.apericraft.apericraft.lib;
+
+public class Reference {
+
+	public static final String MOD_ID = "AC";
+	public static final String  MOD_NAME = "Apericraft";
+	public static final String  VERSION = "Pre-Release  0.0.1";
+}
