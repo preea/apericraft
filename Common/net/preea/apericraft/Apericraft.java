d01006636c3c30cb3df69a76bca93f22c8b02a70
preea <preea03@gmail.com>
2014-03-06 20:55:10 -0500

    Added Copper Ore

--- Common/net/preea/apericraft/Apericraft.java
+++ Common/net/preea/apericraft/Apericraft.java
@@ -11,9 +11,11 @@ package net.preea.apericraft;
  *
  */
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.preea.apericraft.block.CopperOre;
 import net.preea.apericraft.lib.Reference;
 import net.preea.apericraft.proxy.CommonProxy;
-
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -21,10 +23,15 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 
 public class Apericraft {
 	
@@ -34,6 +41,10 @@ public class Apericraft {
 	@SidedProxy(clientSide = "net.preea.apericraft.proxy.ClientProxy", serverSide = "net.preea.apericraft.proxy.CommonProxy")
 	public static CommonProxy proxy;
 	
+	//Block deceleration
+
+	public static Block CopperOre;
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		
@@ -41,9 +52,15 @@ public class Apericraft {
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event){
+	
+		CopperOre = new CopperOre(500, Material.rock).setUnlocalizedName("CopperOre");
+		
+		GameRegistry.registerBlock (CopperOre, Reference.MOD_ID + "_" + CopperOre.getUnlocalizedName().substring(5));
+		
+		LanguageRegistry.addName(CopperOre, "Copper Ore");
 		
 	}
-
+		
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event){
 		
---
+++ Common/net/preea/apericraft/block/CopperOre.java
@@ -0,0 +1,15 @@
+package net.preea.apericraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class CopperOre extends Block{
+
+	public CopperOre(int id, Material material) {
+		super(id, material);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+
+	}
+
+}
